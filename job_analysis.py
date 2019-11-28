30b5fd9b25ba34aaa48d39ab89a5a860ab498854
carlosmd14 <carlosmadrizd@gmail.com>
2019-11-28 11:34:47 +0000

    house is clean

--- job_analysis.py
+++ job_analysis.py
@@ -1,13 +1,56 @@
 from modules import get_job_links, scrape_job_page, open_driver, close_driver
 import os
 import glob
+import pandas as pd
+import csv
 
 def main():
     
     # Getting links:
     get_job_links()
     
-    # Scraping:   
+    # Cleaning links csv:
+    
+    def clean_csv(path):
+
+        print('\nCleaning: ' + path)
+
+        with open(path, 'rt') as f:
+            files = f.read().split(",")
+
+        #, newline='\r\n'    
+
+        files = [file.strip("\"\"") for file in files]
+
+        page_list = []
+
+        for file in files:
+            if len(file) > 45:
+                for second in file.split("\n"):
+                    page_list.append(second.strip("\"\""))
+            else:
+                page_list.append(file)
+
+        page_list = page_list[:-1]
+
+        page_list = list(set(page_list))
+
+        with open(path, 'w', newline='') as myfile:
+            wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+            wr.writerow(page_list)
+    
+    # Cleaning linkedin_search:
+    
+    def clean_search(path):
+        
+        print('\nCleaning: ' + path)
+        df = pd.read_csv(path)
+        df.drop_duplicates(keep='last', inplace=True)
+        df.reset_index(drop=True, inplace=True)
+        df.to_csv(path, index=False)
+    
+    # Scraping:  
+    
     how_many = '0'
     
     while how_many == '0':
@@ -18,12 +61,14 @@ def main():
             job_title = input('\nEnter job title: ')
             job_city = input('\nEnter job location: ')
             path = "job_links/" + job_title.replace(' ', '_') + "_" + job_city.replace(' ', '_') + "_links.csv"
+            clean_csv(path)
             scrape_job_page(path)
             scraped_csv = path
             
         elif how_many == 'all':
             scraped_csv = []
             for path in glob.glob('job_links/*.csv'):
+                clean_csv(path)
                 scrape_job_page(path)
                 scraped_csv.append(path)
             
@@ -33,7 +78,7 @@ def main():
 
     print('\nSuccesfully scraped the following path(s): ' + str(scraped_csv))
 
-    
+    clean_search('linkedin_search.csv')
 
 if __name__ == '__main__':
     main()
